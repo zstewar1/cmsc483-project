fdb90adefa3be3fc1b0b7616d17984c41bfe9a88
Zachary Stewart <chirokidz@gmail.com>
2015-05-04 23:57:48 -0400

    added script for running pciam

--- run_pciam.py
+++ run_pciam.py
@@ -3,14 +3,17 @@ from __future__ import print_function, division
 import collections
 import itertools
 import os
+import json
 import random
+import re
 import subprocess
 import sys
 
-#          0                1     2     3            4         5
-command = ['/usr/bin/time', '-f', '%e', 'bin/pciam', '<IMG1>', '<IMG2>']
+command = ('/usr/bin/time', '-f', '%e', 'bin/pciam',)
 
 ImgPair = collections.namedtuple('ImgPair', ('name', 'i1', 'i2'))
+
+result_parse = re.compile(r'^\(x,y,ncc\) = \((.*), (.*), (.*)\)$')
 
 image_pairs = [
     ImgPair('small', 'img1_sm.tif', 'img2_sm.tif'),
@@ -20,13 +23,32 @@ image_pairs = [
 def main():
     DEVNULL = open(os.devnull, 'w')
     proc = None
-    try:
 
-    except KeyboardInterrupt:
-        if proc is not None and proc.poll is None:
-            proc.terminate()
+    results = {}
+
+    if os.path.exists('results_pciam.json'):
+        with open('results_pciam.json', 'r') as file:
+            results = json.load(file)
+    try:
+        for image_pair in image_pairs:
+            for repeat in range(len(results.setdefault(image_pair.name, {'results':[], 'runtimes':[]})['runtimes']), 20):
+                cmd = command + image_pair[-2:]
+                print(cmd)
+                proc = subprocess.Popen(cmd, stderr=subprocess.PIPE)
+                err = proc.communicate()[1].decode('ascii')
+                out, time = err.splitlines()
+                out = result_parse.match(out)
+                time = float(time)
+                print(out.group(1), out.group(2), out.group(3))
+                out = (int(out.group(1)), int(out.group(2)), float(out.group(3)))
+                results[image_pair.name]['results'].append(out)
+                results[image_pair.name]['runtimes'].append(time)
+                with open('results_pciam.json', 'w') as file:
+                    json.dump(results, file)
     finally:
         DEVNULL.close()
+        if proc is not None and proc.poll is None:
+            proc.terminate()
 
 if __name__ == '__main__':
     main()
